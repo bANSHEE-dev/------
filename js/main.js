16bc161092f34198680d1421eadfa96d5521f113
Andrey Sankovich <nevozmojke@gmail.com>
2020-11-09 21:16:14 +0300

    викинг

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ $(function () {
   //   }
   // });
 
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active')
+  });
 
   $('.heroes__slider-img').slick({
     slidesToShow: 1,
@@ -14,6 +17,14 @@ $(function () {
     asNavFor: '.heroes__slider-text',
     prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
     nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false
+        }
+      },
+    ]
   });
   $('.heroes__slider-text').slick({
     slidesToShow: 1,
